838f14154ff986ae8e0f6456717b376923e075af
hewel <hewelzei@qq.com>
2019-08-27 10:09:44 +0800

    style: add import/no-extraneous-dependencies rule to lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,40 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+  },
+  extends: ["airbnb", "plugin:react/recommended", "plugin:prettier/recommended", "prettier", "prettier/react"],
+  plugins: ["prettier", "emotion"],
+  rules: {
+    semi: "off",
+    "linebreak-style": "off",
+    "import/no-unresolved": [2, { ignore: [`U|utils.*`] }],
+    "react/jsx-indent": [2, 2],
+    "import/extensions": [2, "never", { jsx: "always" }],
+    "import/no-extraneous-dependencies": [2, { devDependencies: ["webpack/**/*", "webpack.*"] }],
+  },
+  overrides: [
+    {
+      files: ["*.jsx", "*.tsx"],
+      plugins: ["react-hooks"],
+      rules: {
+        // 'react/jsx-indent': 'off',
+        // 'prettier/prettier': 'off',
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+        "react/prop-types": [
+          1,
+          {
+            ignore: ["className", "children"],
+          },
+        ],
+      },
     },
-    parserOptions: {
-        ecmaVersion: 2019,
-        sourceType: 'module',
-    },
-    extends: ['airbnb', 'plugin:react/recommended', 'plugin:prettier/recommended', 'prettier', 'prettier/react'],
-    plugins: ['prettier', 'emotion'],
-    rules: {
-        semi: 'off',
-        'linebreak-style': 'off',
-        'import/no-unresolved': [2, { ignore: [`U|utils.*`] }],
-        'react/jsx-indent': [2, 2],
-        'import/extensions': [2, 'never', { jsx: 'always' }],
-    },
-    overrides: [
-        {
-            files: ['*.jsx', '*.tsx'],
-            plugins: ['react-hooks'],
-            rules: {
-                // 'react/jsx-indent': 'off',
-                // 'prettier/prettier': 'off',
-                'react-hooks/rules-of-hooks': 'error',
-                'react-hooks/exhaustive-deps': 'warn',
-                'react/prop-types': [
-                    1,
-                    {
-                        ignore: ['className', 'children'],
-                    },
-                ],
-            },
-        },
-    ],
-}
+  ],
+};
